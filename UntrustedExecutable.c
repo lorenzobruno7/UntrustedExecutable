f813bf9fd49c972aef51e7a0d16885f942623e5f
Lorenzo <134946910+lorenzobruno7@users.noreply.github.com>
2026-04-02 17:31:54 +0200

    Added C and compiled files

---
+++ UntrustedExecutable.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("\n");
+    printf("====================================================\n");
+    printf("[!] Untrusted Execution Test\n");
+    printf("[!] Application Whitelisting is NOT enforced.\n");
+    printf("====================================================\n");
+    printf("\n");
+    
+    return 0;
+}
